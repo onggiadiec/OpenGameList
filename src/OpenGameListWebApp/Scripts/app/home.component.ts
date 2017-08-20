c7b0f6465c3879d7c1443d6ae76fb6ad7abadd03
onggiadiec <baobqvo@gmail.com>
2017-08-19 23:14:29 -0500

    Chapter 6

--- src/OpenGameListWebApp/Scripts/app/home.component.ts
+++ src/OpenGameListWebApp/Scripts/app/home.component.ts
@@ -3,29 +3,21 @@
 @Component({
     selector: "home",
     template: `
-        <h1>{{ title }}</h1>
-        <item-list class="latest"></item-list>
-        <item-list class="most-viewed"></item-list>
-        <item-list class="random"></item-list>
+        <h2>
+            A non-comprehensive directory of open-source video games
+            available on the web
+        </h2>
+        <div class="col-md-4">
+            <item-list class="latest"></item-list>
+        </div>
+        <div class="col-md-4">        
+            <item-list class="most-viewed"></item-list>
+        </div>    
+        <div class="col-md-4">      
+            <item-list class="random"></item-list>
+        </div>    
     `,
-    styles: [`
-        item-list {
-            min-width: 332px;
-            border: 1px solid #aaaaaa;
-            display: inline-block;
-            margin: 0 10px;
-            padding: 10px;
-        }
-        item-list.latest {
-            background-color: #f9f9f9;
-        }
-        item-list.most-viewed {
-            background-color: #f0f0f0;
-        }
-        item-list.random {
-            background-color: #e9e9e9;
-        }
-    `]
+    styles: []
 })
 
 export class HomeComponent {
--- src/OpenGameListWebApp/gulpfile.js
+++ src/OpenGameListWebApp/gulpfile.js
@@ -1,6 +1,7 @@
 ﻿var gulp = require('gulp'),
     gp_clean = require('gulp-clean'),
     gp_concat = require('gulp-concat'),
+    gp_less = require('gulp-less'),
     gp_sourcemaps = require('gulp-sourcemaps'),
     gp_typescript = require('gulp-typescript'),
     gp_uglify = require('gulp-uglify');
@@ -14,18 +15,24 @@ var srcPaths = {
         'node_modules/zone.js/dist/zone.js',
         'node_modules/reflect-metadata/Reflect.js',
         'node_modules/systemjs/dist/system.src.js',
-        'node_modules/typescript/lib/typescript.js'
+        'node_modules/typescript/lib/typescript.js',
+        'node_modules/ng2-bootstrap/bundles/ng2-bootstrap.min.js',
+        'node_modules/moment/moment.js'
     ],
     js_angular: [
         'node_modules/@angular/**'
     ],
     js_rxjs: [
         'node_modules/rxjs/**'
+    ],
+    less: [
+        'Scripts/less/**/*.less'
     ]
 };
 
 var destPaths = {
     app: 'wwwroot/app/',
+    css: 'wwwroot/css/',
     js: 'wwwroot/js/',
     js_angular: 'wwwroot/js/@angular/',
     js_rxjs: 'wwwroot/js/rxjs/'
@@ -44,6 +51,18 @@ gulp.task('app', ['app_clean'], function () {
 // Delete wwwroot/app contents
 gulp.task('app_clean', function () {
     return gulp.src(destPaths.app + "*.*", { read: false })
+    .pipe(gp_clean({ force: true }));
+});
+
+// Process all LESS files and output the resulting CSS in wwwroot/css
+gulp.task('less', ['less_clean'], function () {
+    return gulp.src(srcPaths.less)
+    .pipe(gp_less())
+    .pipe(gulp.dest(destPaths.css));
+});
+// Delete wwwroot/css contents
+gulp.task('less_clean', function () {
+    return gulp.src(destPaths.css + "*.*", { read: false })
     .pipe(gp_clean({ force: true }));
 });
 
@@ -69,7 +88,7 @@ gulp.task('watch', function () {
 });
 
 // Global cleanup task
-gulp.task('cleanup', ['app_clean', 'js_clean']);
+gulp.task('cleanup', ['app_clean', 'js_clean', 'less_clean']);
 
 // Define the default task so it will launch all other tasks
-gulp.task('default', ['app', 'js', 'watch']);
+gulp.task('default', ['app', 'js', 'less', 'watch']);
